8360e1277bcca451092af4c77c42349cf20fcbc7
Francisco Meneses <fmeneses@redhat.com>
2023-02-21 13:02:34 -0300

    Performance fixes

--- config.d.ts
+++ config.d.ts
@@ -1,28 +1,19 @@
 import { TaskScheduleDefinitionConfig } from '@backstage/backend-tasks';
 
 interface ThreeScaleConfig {
-  
   baseUrl: string;
-
   serviceAccount: string;
-
   serviceAccountCredentials: string;
-
   systemLabel?: string;
-
   ownerLabel?: string;
-
   addLabels?: boolean
-
   schedule?: TaskScheduleDefinitionConfig;
 }
 
 export interface Config {
   catalog?: {
-
     providers?: {
-
-      threeScaleApiEntity?: Record<string, ThreeScaleConfig>;
+      threeScaleApiEntity?: { [key: string]: ThreeScaleConfig };
     };
   };
 }
--- src/providers/config.ts
+++ src/providers/config.ts
@@ -3,21 +3,13 @@ import { Config } from '@backstage/config';
 import { ThreeScaleConfig } from './types';
 
 export function readThreeScaleApiEntityConfigs(config: Config): ThreeScaleConfig[] {
-  const configs: ThreeScaleConfig[] = [];
-
   const providerConfigs = config.getOptionalConfig(
     'catalog.providers.threeScaleApiEntity',
   );
-
   if (!providerConfigs) {
-    return configs;
+    return [];
   }
-
-  for (const id of providerConfigs.keys()) {
-    configs.push(readThreeScaleApiEntityConfig(id, providerConfigs.getConfig(id)));
-  }
-
-  return configs;
+  return providerConfigs.keys().map(id => readThreeScaleApiEntityConfig(id, providerConfigs.getConfig(id)))
 }
 
 function readThreeScaleApiEntityConfig(id: string, config: Config): ThreeScaleConfig {
